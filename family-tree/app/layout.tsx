75c162fa0586f37b2c3845d8ffac9fa59c26fe38
peekaycee <kalukaluokomba@gmail.com>
2025-12-29 21:40:09 +0100

    update latest version 29.12.25 9:40PM

--- family-tree/app/layout.tsx
+++ family-tree/app/layout.tsx
@@ -6,27 +6,27 @@ import Footer from '../components/Footer';
 import ClientProtectedWrapper from './ClientProtectedWrapper';
 import { ReactNode } from 'react';
 import PageTransition from './PageTransition';
+import { ToastProvider } from './ToastContext';
 
 export const metadata = {};
-export const viewport = { width: "device-width", initialScale: 1 };
-export const dynamic = "force-dynamic";
+export const viewport = { width: 'device-width', initialScale: 1 };
+export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={styles.body}>
-        <Navbar />
-
-        {/* Client-side wrapper only around main content */}
-        <ClientProtectedWrapper>
-          <PageTransition>
-            <main>{children}</main>
-          </PageTransition>
-        </ClientProtectedWrapper>
-
+        <ToastProvider>
+          <Navbar />
+          <ClientProtectedWrapper>
+            <PageTransition>
+              <main>{children}</main>
+            </PageTransition>
+          </ClientProtectedWrapper>
+        </ToastProvider>
         <Footer />
       </body>
     </html>
-  )
+  );
 }

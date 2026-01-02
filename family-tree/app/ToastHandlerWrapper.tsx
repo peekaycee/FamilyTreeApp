67f6d28ab9ba070a8d9bf8388556a952b42d0f84
peekaycee <kalukaluokomba@gmail.com>
2026-01-02 23:26:49 +0100

    update latest version 11:27pm 2.01.26.

--- family-tree/app/ToastHandlerWrapper.tsx
+++ family-tree/app/ToastHandlerWrapper.tsx
@@ -10,7 +10,7 @@ export default function ToastHandlerWrapper({ children }: { children: ReactNode 
     const pending = localStorage.getItem("pending_toast");
     if (pending) {
       const { message, type } = JSON.parse(pending);
-      showToast(message, type);
+      showToast({message, type});
       localStorage.removeItem("pending_toast");
     }
   }, [showToast]);

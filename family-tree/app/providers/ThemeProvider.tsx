d3f7f2294ccdcb56d2046bfa0816c798cf22442e
peekaycee <kalukaluokomba@gmail.com>
2026-05-02 02:34:50 +0100

    Checking PR

--- family-tree/app/providers/ThemeProvider.tsx
+++ family-tree/app/providers/ThemeProvider.tsx
@@ -3,6 +3,7 @@
 import { useEffect } from "react";
 import { useSettings } from "@/app/hooks/useSettings";
 import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 export default function ThemeProvider({ children }: { children: React.ReactNode }) {
   const { state, isHydrated } = useSettings();
@@ -30,5 +31,9 @@ export default function ThemeProvider({ children }: { children: React.ReactNode 
 
   }, [state.theme, state.accentColor, isHydrated, isPublicPage]);
 
-  return <>{children}</>;
+  return (
+  <>
+    <Link href="/">Link</Link>
+    {children}
+  </>);
 }
